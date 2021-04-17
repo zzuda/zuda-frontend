9bb6ce6fdb41df1045556a6b8b8cc4e74de2f1e8
Twince <tjsdn811@gmail.com>
2021-04-18 02:06:42 +0900

    feat: update

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ParallaxBG from '../assets/ParallaxBG';
+import Main from '../components/Main';
 import Login from '../components/Login';
 
 const MainPage = () => {
   return (
     <ParallaxBG>
-      <Login />
+      <Main />
     </ParallaxBG>
   );
 };
