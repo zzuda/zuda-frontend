4453eb2e2aaab3dbb70badbe6f9ad2e7023421bd
ArKc1009 <arkc1009@gmail.com>
2021-04-17 23:40:44 +0900

    test: Login 컴포넌트 테스트

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ParallaxBG from '../assets/ParallaxBG';
+import styled from 'styled-components';
+import Login from '../components/Login';
 
 const MainPage = () => {
-  return <ParallaxBG></ParallaxBG>;
+  return (
+    <ParallaxBG>
+      <Login />
+    </ParallaxBG>
+  );
 };
 
 export default MainPage;
