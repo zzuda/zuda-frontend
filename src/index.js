109c5cbdb6e6cbebec9c99044a2e7c8634800537
ArKc1009 <arkc1009@gmail.com>
2021-04-17 21:28:46 +0900

    feat: 프로젝트 구조 짜기

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './styles/global.css';
+import MainPage from './pages/MainPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MainPage />
   </React.StrictMode>,
   document.getElementById('root'),
 );
