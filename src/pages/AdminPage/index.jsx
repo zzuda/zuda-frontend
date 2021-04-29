784f7c9057888c66500dd416256e5ed75952a782
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-04-29 14:47:50 +0900

    feat: 어드민 페이지 생성

---
+++ src/pages/AdminPage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+const SideBar = styled.div`
+  width: 20%;
+`;
+
+const AdminPage = () => {
+  return <Container>Admin!</Container>;
+};
+
+export default AdminPage;
