4ae308fa30cb9cb632cdee06fe7a9a3316a7b2dd
ArKc1009 <arkc1009@gmail.com>
2021-05-01 23:06:19 +0900

    feat: token 관리 함수 모듈 생성

---
+++ src/Api/tokenManage.jsx
@@ -0,0 +1,12 @@
+export const getToken = () => {
+  return localStorage.getItem('token');
+};
+
+export const setToken = (token) => {
+  localStorage.setItem('token', token);
+};
+
+export const removeToken = () => {
+  localStorage.removeItem('token');
+  localStorage.clear();
+};
