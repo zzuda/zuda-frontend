9ba7d5c51db9db721c243f17d22007b2d43f46be
Sunwoo Ha <tjsdn811@gmail.com>
2021-05-06 16:54:32 +0900

    feat: publising

---
+++ src/components/Admin/MenuBar/MainMenu.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from  'styled-components';
+
+const Container = styled.div`
+    width: 100%;
+    height: 77%;
+    background-color: #e8e8e8;
+`;
+
+const SearchBar = styled.input`
+    border: none;
+    
+    margin: 0 auto;
+    margin-top:20px;
+    margin-left: 105px;
+`;
+
+const MenuText = styled.li`
+    text-align: center;
+    
+    font-size: 28px;
+    color: #555;
+`;
+
+
+const MainMenu = () => {
+    return(,
+        <Container>
+            <SearchBar/>
+            <ul>
+                <MenuText>fasdfadsfasf</MenuText>
+                <MenuText>fasdfadsfasf</MenuText>
+                <MenuText>fasdfadsfasf</MenuText>
+            </ul>
+        </Container>
+    )
+}
+export default MainMenu;
