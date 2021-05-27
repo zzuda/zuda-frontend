d7974df24c24a522ca39c404985fd2dfafb9366b
ArKc1009 <arkc1009@gmail.com>
2021-05-28 03:34:50 +0900

    feat: socket Context 구현

---
+++ src/Providers/SocketProvider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SocketContext from '../Contexts/socket';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:8080/room');
+
+const SocketProvider = ({ children }) => {
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
+
+export default SocketProvider;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global.css';
 import MainPage from './pages/MainPage';
+import SocketProvider from './Providers/SocketProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <MainPage />
+    <SocketProvider>
+    </SocketProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
