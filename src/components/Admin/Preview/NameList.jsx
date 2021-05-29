342e5df6a414feaf533669bbb90c2fd0044687b2
Twince <tjsdn811@gmail.com>
2021-05-30 02:55:03 +0900

    feat: Publishing

--- src/components/Admin/Preview/NameList.jsx
+++ src/components/Admin/Preview/NameList.jsx
@@ -1,2 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Frame = styled.div`
+
+`;
+
+const TitleText = styled.h3`
+
+`;
+
+const All = styled.div`
+
+`;
+
+const Room = styled.div`
+    
+`;
+
+
+const NameList = () => {
+    return(
+        <Frame>
+            <All>
+                <TitleText></TitleText>
+                <div></div>
+            </All>
+            <Room>
+                
+            </Room>
+        </Frame>
+    );
+};
+
+export default NameList;
