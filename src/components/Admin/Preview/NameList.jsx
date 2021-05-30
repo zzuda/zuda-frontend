6a34c916addcf1992c2e2587567a64767e979d02
Twince <tjsdn811@gmail.com>
2021-05-31 04:19:25 +0900

    feat: publishing

--- src/components/Admin/Preview/NameList.jsx
+++ src/components/Admin/Preview/NameList.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Frame = styled.div`
-
+    width: 385px;
+    
+    margin: 45px 10px;
 `;
 
 const TitleText = styled.h3`
-
+    color: #585858;
+    font-size: 27px;
 `;
 
 const All = styled.div`
+    display: flex;
+    justify-content: space-between;
 
+    margin-right: 90px;
+    line-height: 50%;
+    
+`;
+
+const Count = styled.span`
+    color: #717171;
+    
+    font-size: 16px;
+    font-weight: 600;
 `;
 
 const Room = styled.div`
@@ -19,11 +34,14 @@ const Room = styled.div`
 
 
 const NameList = () => {
+
+    const [UserCount, setUserCount] = useState(129)
+
     return(
         <Frame>
             <All>
-                <TitleText></TitleText>
-                <div></div>
+                <TitleText>전체</TitleText>
+                <Count>{ UserCount }</Count>
             </All>
             <Room>
                 
