1a215ac2b9f364284a6cbfe9ff8330dbf19c7e6d
ArKc1009 <arkc1009@gmail.com>
2021-06-04 05:09:57 +0900

    feat: 방 퇴장시 방 정보 state 초기화

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -1,11 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect } from 'react';
 import SocketContext from '../contexts/socket';
 import RoomInfoContext from '../contexts/roomInfo';
 import { Redirect } from 'react-router-dom';
 import RedButton from '../atomic/buttons/RedButton';
 
 const RoomPage = () => {
-  const info = useContext(RoomInfoContext)[0];
+  const [info, setInfo] = useContext(RoomInfoContext);
   const socket = useContext(SocketContext);
 
   const onClickQuit = () => {
@@ -13,20 +13,19 @@ const RoomPage = () => {
       return;
     }
     socket.emit('quit', { roomId: info.roomInfo.roomId, questId: info.id });
-    console.log(info);
   };
 
+  const handleQuit = useCallback(() => {
+    setInfo(null);
+  }, [setInfo]);
+
   useEffect(() => {
-    socket.on('quit', (res) => {
-      console.log(res);
-    });
+    socket.on('quit', handleQuit);
 
     return () => {
-      socket.off('quit', (res) => {
-        console.log(res);
-      });
+      socket.off('quit', handleQuit);
     };
-  }, [socket]);
+  }, [socket, handleQuit]);
 
   useEffect(() => {
     socket.on('exception', (err) => {
