746c86b61d55c68d085d40db84a26d510b5b7e17
ArKc1009 <arkc1009@gmail.com>
2021-06-04 04:30:05 +0900

    feat: 방 접속시 페이지 생성

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import SocketContext from '../contexts/socket';
+import RoomInfoContext from '../contexts/roomInfo';
+import { Redirect } from 'react-router-dom';
+import RedButton from '../atomic/buttons/RedButton';
 
 const RoomPage = () => {
-  return <div>sdsa</div>;
+  const info = useContext(RoomInfoContext)[0];
+  const socket = useContext(SocketContext);
+
+  const onClickQuit = () => {
+    if (!info) {
+      return;
+    }
+    socket.emit('quit', { roomId: info.roomInfo.roomId, questId: info.id });
+    console.log(info);
+  };
+
+  useEffect(() => {
+    socket.on('quit', (res) => {
+      console.log(res);
+    });
+
+    return () => {
+      socket.off('quit', (res) => {
+        console.log(res);
+      });
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on('exception', (err) => {
+      console.log(err);
+    });
+
+    socket.off('exception', (err) => {
+      console.log(err);
+    });
+  }, [socket]);
+
+  if (!info) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div>
+      <h3>{info.id}</h3>
+      <div>{info.roomInfo.roomName}</div>
+      <RedButton onClick={onClickQuit}>Quit</RedButton>
+    </div>
+  );
 };
 
 export default RoomPage;
