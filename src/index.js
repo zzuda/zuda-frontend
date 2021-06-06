f3820bd3d458000b6caf31d257bfbb0dca9c1a31
Twince <tjsdn811@gmail.com>
2021-06-07 02:16:53 +0900

    feat: toast_msg settings

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global.css';
 import MainPage from './pages/MainPage';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.render(
   <React.StrictMode>
     <MainPage />
+    <Toaster />
   </React.StrictMode>,
   document.getElementById('root'),
 );
