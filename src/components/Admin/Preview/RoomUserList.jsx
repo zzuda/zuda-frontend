1a763c76110f11810dd4bde30be1548ea6a00c58
Twince <tjsdn811@gmail.com>
2021-06-06 16:26:32 +0900

    feat: Get RoomData

--- src/components/Admin/Preview/RoomUserList.jsx
+++ src/components/Admin/Preview/RoomUserList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from 'styled-components';
 import Api from '../../../Api/index';
 import name from 'module';
@@ -10,22 +10,51 @@ const TitleText = styled.h3`
     color: #585858;
     font-size: 27px;
 `;
+const Wrap = styled.div`
+
+`;
 
 const RoomUserList = () => {
+    const [roomName, setRoomName] = useState([]);
+    console.log(roomName);
+
     useEffect(() => {
-        console.log("get시도!");
+        console.log("get시도");
 
         const GetRoomData = async () =>{
-            const res = await Api.get('http://localhost:8080/room');
-            console.log(res.data.data[0].roomName);
+            const res = await Api.get('/room');
+            const responseData = res.data.data
+            
+            responseData.forEach((value, index) => {
+                setRoomName((prevRoomName) => ([
+                    ...prevRoomName, {
+                        "id" : index, 
+                        "data" : value.roomName
+                    }
+                ]));
+            });
         }
         GetRoomData();
     }, []);
 
+    const roomData = () => {
+        return(
+                roomName.map(x => 
+                    <Wrap>
+                        <TitleText>
+                            {x.data}
+                            </TitleText>
+                    </Wrap>
+                )
+        )
+    }
+
     return (
     <>
         <Container>
-            <TitleText></TitleText>
+            <TitleText>
+                <div>{roomData()}</div>
+            </TitleText>
         </Container>
     </>
     )
