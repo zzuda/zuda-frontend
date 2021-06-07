01ad16a80a0d3b14ddd21a075d0ea40f5f132b74
ArKc1009 <arkc1009@gmail.com>
2021-06-08 05:04:28 +0900

    feat: Card 틀 제작 및 경로 변경

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 
 export const Card = styled.div`
-  width: ${(props) => props.width}px;
-  height: ${(props) => props.height}px;
+  width: ${(props) => props.w}px;
+  height: ${(props) => props.h}px;
   background-color: #f6f6f6;
   box-shadow: 3px 3px 24px #c9c9c9, -18px -18px 36px #ffffff;
   border-radius: 30px;
+  position: relative;
 `;
 
 export const InnerCard = styled.div`
-  width: ${(props) => props.width}px;
-  height: ${(props) => props.height}px;
+  width: ${(props) => props.w}px;
+  height: ${(props) => props.h}px;
   background-color: #eee;
   border-radius: 30px;
+  position: relative;
 `;
