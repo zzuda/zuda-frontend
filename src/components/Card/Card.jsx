69963539fc367c631919824890514ecc1d310bbd
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-06-09 10:25:19 +0900

    feat: Card grid props 추가

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,6 +7,8 @@ export const Card = styled.div`
   box-shadow: 3px 3px 24px #c9c9c9, -18px -18px 36px #ffffff;
   border-radius: 30px;
   position: relative;
+  grid-row: ${(props) => props.rStart / props.rEnd};
+  grid-column: ${(props) => props.cStart / props.cEnd};
 `;
 
 export const InnerCard = styled.div`
@@ -15,11 +17,13 @@ export const InnerCard = styled.div`
   background-color: #eee;
   border-radius: 30px;
   position: relative;
+  grid-row: ${(props) => props.rStart / props.rEnd};
+  grid-column: ${(props) => props.cStart / props.cEnd};
 `;
 
 export const InnerText = styled.h3`
   font-family: 'NanumSquare', sans-serif;
-  font-size: 14px;
+  font-size: ${(props) => (props.fSize ? props.fSize : 14)}px;
   color: #919191;
   position: absolute;
   top: ${(props) => props.top}px;
