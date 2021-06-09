d00b28f658af6bf11c98be559d2bd250772c630e
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-06-09 10:26:32 +0900

    feat: SummaryCard 구현중

--- src/components/Card/Room/SummaryCard/index.jsx
+++ src/components/Card/Room/SummaryCard/index.jsx
@@ -2,20 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 import { Card } from '../../Card';
 import UploadedFile from './UploadedFile';
+import ScreenView from './ScreenView';
+import WorkLog from './WorkLog';
 
 const Container = styled.div`
   padding: 3rem;
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: repeat(2, minmax(100px, auto));
+  grid-template-rows: repeat(2, minmax(10px, auto));
+  grid-gap: 0.1rem;
+  justify-content: stretch;
   align-items: center;
+  grid-gap: 0.4rem;
 `;
 
 const SummaryCard = ({ name }) => {
   return (
     <Card w={557} h={770}>
+      {name}
       <Container>
-        {name}
-        <UploadedFile />
+        <UploadedFile cStart={1} cEnd={3} rStart={1} rEnd={2} />
+        <ScreenView cStart={1} cEnd={2} />
+        <WorkLog />
       </Container>
     </Card>
   );
