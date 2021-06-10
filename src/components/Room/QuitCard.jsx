02b0ff1c542fa2cb705350c5348d80ce48a65b7d
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-06-10 16:53:39 +0900

    feat: 퇴장버튼 퍼블리싱

---
+++ src/components/Room/QuitCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Card } from '../Card/Card';
+import QuitIcon from '../../assets/Icons/exit.png';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+  cursor: pointer;
+  transition: transform 0.3s;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const Icon = styled.img`
+  background-color: #f6f6f6;
+`;
+
+const QuitCard = ({ rStart, rEnd, cStart, cEnd }) => {
+  return (
+    <Card
+      w={96}
+      h={136}
+      rStart={rStart}
+      rEnd={rEnd}
+      cStart={cStart}
+      cEnd={cEnd}
+    >
+      <Container>
+        <Icon src={QuitIcon} alt="Quit" />
+      </Container>
+    </Card>
+  );
+};
+
+export default QuitCard;
