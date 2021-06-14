47ead493fd9189d05692993da27645a8794b459c
Sunwoo Ha <tjsdn811@gmail.com>
2021-06-14 13:15:21 +0900

    feat: Design Refectoring

--- src/components/Admin/RoomBlock/RoomSection.jsx
+++ src/components/Admin/RoomBlock/RoomSection.jsx
@@ -25,13 +25,13 @@ const RoomBlock = styled.div`
     width: 390px;
     height: 380px;
 
-    padding-top: 2em;
-    padding-left: 2.4em;
+    padding-top: 0.7em;
+    padding-left: 1.2em;
     padding-right: 2.4em;
     
     margin-top: 50px;
 
-    border-radius: 40px;
+    border-radius: 18px;
     background-color: #ececec;
 
     /* animation-duration: 0.3s all; */
@@ -40,7 +40,7 @@ const RoomBlock = styled.div`
         transform: scale(1.03);
         box-shadow: 34px 26px 94px -10px #00000030;
         -webkit-box-shadow: 34px 26px 94px -10px #00000036;
-        -moz-box-shadow: 34px 26px 94px -10px rgba(0,0,0,0.67);
+        -moz-box-shadow: 34px 26px 94px -10px #0000007d;
 
         background-color: #e9e9e9;
     }
@@ -76,23 +76,41 @@ const Content = styled.div`
 `;
 
 const PersonCount = styled.div`
-    width: 100%;
-    height: 80px;
+    width: 350px;
+    height: 90px;
+
+    display: flex;
+    align-items: center;
 
     margin-top: 10px;
 
-    border-radius: 25px;
+    border-radius: 18px;
 
     background-color: #fefefe;
 `;
 
 const UserCount = styled.img`
-    width: 35px;
-    height: 35px;
+    width: 40px;
+    height: 40px;
 
-    margin-top: 20px;
+    margin: 0 15px 0 20px;
+`;
+
+const CurrentUserText = styled.span`
+    font-size: 31px;
+    font-weight: bold;
+    color: #6e6e6e;
+
+    margin-bottom: 3px;
+`;
+
+const Percentage = styled.div`
+    width: 150px;
+    height: 20px;
+
     margin-left: 20px;
-
+    border-radius: 5px;
+    background-color: #ececec;
 `;
 
 
@@ -106,7 +124,9 @@ const RoomSection = () => {
                     </Title>
                     <Content>
                         <PersonCount>
-                           <UserCount src={UserCountImg}/>
+                            <UserCount src={UserCountImg}/>
+                            <CurrentUserText>23명</CurrentUserText>
+                            <Percentage></Percentage>
                         </PersonCount>
                     </Content>
                 </RoomBlock>
