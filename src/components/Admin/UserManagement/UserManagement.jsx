9a2e303bbf1339ed8bd6969ffdfa881e35c8b4a5
Sunwoo Ha <tjsdn811@gmail.com>
2021-06-22 16:41:35 +0900

    feat: Publishing

--- src/components/Admin/UserManagement/UserManagement.jsx
+++ src/components/Admin/UserManagement/UserManagement.jsx
@@ -45,6 +45,58 @@ const UserCount = styled.p`
     color: #414146;
 `;
 
+
+const UserSection = styled.div`
+    padding-top: 35px;
+`;
+
+const UserBox = styled.div`
+    position: relative;
+
+    width: 90%;
+    height: 70px;
+
+    display: flex;
+    align-items: center;
+
+    border-radius: 16px;
+    background-color: #eeeeee;
+
+    -webkit-box-shadow: 13px 13px 25px -2px rgba(0,0,0,0.16); 
+    box-shadow: 13px 13px 25px -2px rgba(0,0,0,0.16);
+
+    transition: 0.3s ease-in-out;
+    &:hover{
+        transform: scale(1.01);
+    }
+`;
+
+const Icon = styled.div`
+    width: 50px;
+    height: 50px;
+
+    margin-left: 23px;
+
+    background-color: #fff;
+    border-radius: 20px;
+
+    font-size: 21px;
+    color: #5a5a5a;
+    
+    padding-left: 13px;
+    line-height: 50px;
+`;
+
+const UserName = styled.div`
+    margin-left: 20px;
+    margin-bottom: 2px;
+    
+    font-size: 30px;
+    font-weight: 700;
+
+    color: #5a5a5a;
+`;
+
 const UserManagement = () => {
     return (
         <Container>
@@ -54,6 +106,12 @@ const UserManagement = () => {
                     <TitleLine />
                     <UserCount>23</UserCount>
                 </TitleSection>
+                <UserSection>
+                    <UserBox>
+                        <Icon>22</Icon>
+                        <UserName>하대겸</UserName>
+                    </UserBox>
+                </UserSection>
             </Room>
         </Container>
     )
