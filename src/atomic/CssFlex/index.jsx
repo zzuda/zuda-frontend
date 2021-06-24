0d1af6c32d39bf5a5a6c49a1c08d904936695429
Sunwoo Ha <tjsdn811@gmail.com>
2021-06-24 10:18:50 +0900

    feat: Flex Css(Atomic)

---
+++ src/atomic/CssFlex/index.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const FlexWrap = styled.div`
+    display: flex;
+    flex-direction: ${(props) => (props.direction ? props.direction : "row")};
+    justify-content: space-${(props) => props.mode};
+`;
