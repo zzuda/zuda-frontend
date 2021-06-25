30e198fc8fc8592825cdf2679dde93a7258b1397
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-06-25 13:34:47 +0900

    feat: FileList 컴포넌트 style

--- src/components/Room/FileShare/FileList.jsx
+++ src/components/Room/FileShare/FileList.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  width: 600px;
+  height: 600px;
+  border: 3px solid #ccc;
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const FileList = () => {
-  return <div></div>;
+  return <Container>FileList</Container>;
 };
 
 export default FileList;
