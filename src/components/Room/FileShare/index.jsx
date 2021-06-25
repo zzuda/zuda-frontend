4715180f4555e230ba33c2fd8e28b088b0d2303c
arkc1009 <52207750+arkc1009@users.noreply.github.com>
2021-06-25 13:34:14 +0900

    feat: FileShareModal Content 틀 및 UploadFile 컴포넌트 추가

--- src/components/Room/FileShare/index.jsx
+++ src/components/Room/FileShare/index.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 import FileShareHeader from './FileShareHeader';
 import FileList from './FileList';
+import UploadFile from './UploadFile';
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
+  padding: 2rem;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1rem;
 `;
 
 const FileShare = () => {
   return (
     <Container>
       <FileShareHeader />
-      <FileList />
+      <Content>
+        <FileList />
+        <UploadFile />
+      </Content>
     </Container>
   );
 };
