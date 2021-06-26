6f0b81e7c6328feb9008ee62b20e33d284009cce
Twince <tjsdn811@gmail.com>
2021-06-27 00:24:35 +0900

    chore: Css 변경

--- src/components/Admin/Preview/RoomUserList.jsx
+++ src/components/Admin/Preview/RoomUserList.jsx
@@ -21,13 +21,17 @@ const TitleText = styled.h3`
 `;
 
 const Wrap = styled.div`
-    margin: 3rem 0 0 0.8rem;
+    margin: 2rem 0 0 0.8rem;
+    
+    &:nth-child(1){
+        margin-top: 30px;
+    }
 `;
 
 const TitleAndCount = styled.div`
     display: flex;
     justify-content: space-around;
-    line-height: 50%;
+    align-items: center;
 
 `;
 
@@ -37,6 +41,15 @@ const RoomLine = styled.div`
 
     margin-top: 8px;
     background-color: #bdbdbd;
+`;
+
+const UserName = styled.p`
+    font-family: 'NanumSquare', sans-serif;
+    font-size: 20px;
+    font-weight: 400;
+    color: #b7b7b7;
+
+    margin: 18px 17px;
 `;
 
 const RoomUserList = () => {
@@ -62,25 +75,25 @@ const RoomUserList = () => {
         GetRoomData();
     }, []);
 
-    const roomData = () => {
-        return(
-                roomName.map(x => 
-                    <Wrap>
-                        <TitleAndCount>
-                            <TitleText>{x.data}</TitleText>
-                            <RoomLine width={90}></RoomLine>
-                            <CountText>26</CountText>
-                        </TitleAndCount>
-                    </Wrap>
-                )
-        )
-    }
+    const User = ["하건우", "하대겸", "하선우", "한동진"];
+    const UserList = User.map(i => <UserName># {i}</UserName>)
+
+    const roomData =  roomName.map(x => 
+        <Wrap>
+            <TitleAndCount>
+                <TitleText>{x.data}</TitleText>
+                <RoomLine width={90}></RoomLine>
+                <CountText fSize={13} marginTop={5}>26명</CountText>
+            </TitleAndCount>
+            <span>{UserList}</span>
+        </Wrap>
+    )
 
     return (
     <>
         <Container>
             <TitleText>
-                <>{roomData()}</>
+                <>{roomData}</>
             </TitleText>
         </Container>
     </>
