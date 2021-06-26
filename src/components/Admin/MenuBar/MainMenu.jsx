aacda690ff9497f24143c7cd0491d2d746287010
Twince <tjsdn811@gmail.com>
2021-06-27 00:55:33 +0900

    chore: prettier

--- src/components/Admin/MenuBar/MainMenu.jsx
+++ src/components/Admin/MenuBar/MainMenu.jsx
@@ -1,101 +1,98 @@
 import React from 'react';
-import styled from  'styled-components';
+import styled from 'styled-components';
 
 const Container = styled.div`
-    width: 100%;
-    height: 77%;
-    background-color: #e8e8e8;
-    padding-top: 90px;
+  width: 100%;
+  height: 77%;
+  background-color: #e8e8e8;
+  padding-top: 90px;
 `;
 
 const SearchBar = styled.input`
-    margin: 0 auto;
-    
-    border: 0;
-    border-bottom: 1.5px solid #949494;
-    display: block;
+  margin: 0 auto;
 
-    width: 250px;
-    height: 50px;
+  border: 0;
+  border-bottom: 1.5px solid #949494;
+  display: block;
 
-    background-color: #e8e8e8;
+  width: 250px;
+  height: 50px;
 
-    cursor: pointer;
+  background-color: #e8e8e8;
 
-    &:hover {
-        background-color: #e0e0e0;
-        transition: 0.3s ease-in-out;
-    }
-    &:focus {
-        outline: none;
-    }
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e0e0e0;
+    transition: 0.3s ease-in-out;
+  }
+  &:focus {
+    outline: none;
+  }
 `;
 
-
 const MenuFrame = styled.ul`
-    margin: 0 auto;
-    margin-top: 4em;
+  margin: 0 auto;
+  margin-top: 4em;
 
-    width: 250px;
-    height: 130px;
+  width: 250px;
+  height: 130px;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const MenuText = styled.li`
-    list-style: none;
-    
-    font-size: 26px;
-    font-weight: 600;
-    color: #555;
+  list-style: none;
 
-    cursor: pointer;
-    user-select: none;
+  font-size: 26px;
+  font-weight: 600;
+  color: #555;
 
-    transition: 0.3s;
-    &:hover{
-        color: #ff4141;
-        border-right: 3px solid #ff4141;
+  cursor: pointer;
+  user-select: none;
 
-        transform: translate(4px);
-    }
+  transition: 0.3s;
+  &:hover {
+    color: #ff4141;
+    border-right: 3px solid #ff4141;
+
+    transform: translate(4px);
+  }
 `;
 
 const HelpMenuFrame = styled.ul`
-    margin: 0 auto;
-    margin-top: 7rem;
+  margin: 0 auto;
+  margin-top: 7rem;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
-    width: 240px;
-    height: 100px;
+  width: 240px;
+  height: 100px;
 `;
 
 const HelpText = styled.p`
-    color: #717171;
-    font-weight: 600;
+  color: #717171;
+  font-weight: 600;
 `;
 
-
 const MainMenu = () => {
-    return(
-        <Container>
-            <SearchBar></SearchBar>
-            <MenuFrame>
-                <MenuText>방관리</MenuText>
-                <MenuText>파일 공유 관리</MenuText>
-            </MenuFrame>
-            <HelpMenuFrame>
-                <HelpText>도움말</HelpText>
-                <HelpText>주다 서비스</HelpText>
-                <HelpText>연락처</HelpText>
-            </HelpMenuFrame>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <SearchBar></SearchBar>
+      <MenuFrame>
+        <MenuText>방관리</MenuText>
+        <MenuText>파일 공유 관리</MenuText>
+      </MenuFrame>
+      <HelpMenuFrame>
+        <HelpText>도움말</HelpText>
+        <HelpText>주다 서비스</HelpText>
+        <HelpText>연락처</HelpText>
+      </HelpMenuFrame>
+    </Container>
+  );
+};
 export default MainMenu;
