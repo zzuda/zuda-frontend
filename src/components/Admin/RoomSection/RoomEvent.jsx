07b3adfacf9cbc92c8087c45ccc62a0728765f18
Twince <tjsdn811@gmail.com>
2021-06-27 00:55:18 +0900

    chore: prettier

--- src/components/Admin/RoomSection/RoomEvent.jsx
+++ src/components/Admin/RoomSection/RoomEvent.jsx
@@ -1,71 +1,70 @@
-import React from "react";
-import styled from "styled-components";
-import { FlexWrap } from "../../../atomic/CssFlex";
+import React from 'react';
+import styled from 'styled-components';
+import { FlexWrap } from '../../../atomic/CssFlex';
 
 const RoomEventSection = styled.div`
-    cursor: pointer;
+  cursor: pointer;
 
-    width: 180px;
-    height: 55px;
+  width: 180px;
+  height: 55px;
 
-    margin-top: 14px;
+  margin-top: 14px;
 
-    border-radius: 15px;
-    background-color: #fff;
+  border-radius: 15px;
+  background-color: #fff;
 `;
 
 const ChangeRoomName = styled.div`
-    width: 90px;
-    height: 55px;
+  width: 90px;
+  height: 55px;
 
-    border-right: 1px solid #e2e2e2;
-    border-radius:15px 0 0 15px;
+  border-right: 1px solid #e2e2e2;
+  border-radius: 15px 0 0 15px;
 
-    transition: 0.3s ease-in-out;
+  transition: 0.3s ease-in-out;
 
-    font-weight:700;
-    font-size: 14px;
-    
-    line-height: 55px;
-    text-align: center;
+  font-weight: 700;
+  font-size: 14px;
 
-    color: #535353;
-    &:hover{
-        background-color: #e2e2e2;
-    }
+  line-height: 55px;
+  text-align: center;
+
+  color: #535353;
+  &:hover {
+    background-color: #e2e2e2;
+  }
 `;
 
 const DeleteRoom = styled.div`
-    width: 90px;
-    height: 55px;
-    
-    border-radius:0 15px 15px 0;
-    
-    transition: 0.3s ease-in-out;
+  width: 90px;
+  height: 55px;
 
-    font-weight:700;
-    font-size: 14px;
-    
-    line-height: 55px;
-    text-align: center;
+  border-radius: 0 15px 15px 0;
 
-    color: #ff5c5c;
-    &:hover{
-        border: 1px solid #ff6565;
-        background-color: #ffbbbb;
-    }
+  transition: 0.3s ease-in-out;
+
+  font-weight: 700;
+  font-size: 14px;
+
+  line-height: 55px;
+  text-align: center;
+
+  color: #ff5c5c;
+  &:hover {
+    border: 1px solid #ff6565;
+    background-color: #ffbbbb;
+  }
 `;
 
-
 const RoomEvent = () => {
-    return(
-        <RoomEventSection>
-            <FlexWrap>
-                <ChangeRoomName>방 이름변경</ChangeRoomName>
-                <DeleteRoom>방 삭제</DeleteRoom>
-            </FlexWrap>
-        </RoomEventSection>
-    )
-}
+  return (
+    <RoomEventSection>
+      <FlexWrap>
+        <ChangeRoomName>방 이름변경</ChangeRoomName>
+        <DeleteRoom>방 삭제</DeleteRoom>
+      </FlexWrap>
+    </RoomEventSection>
+  );
+};
 
 export default RoomEvent;
--- src/components/Admin/RoomSection/Title.jsx
+++ src/components/Admin/RoomSection/Title.jsx
@@ -1,35 +1,34 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const TitleSection = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
 `;
 
 const RoomName = styled.span`
-    color: #414141;
-    
-    font-size: 23px;
-    font-weight: 800;
+  color: #414141;
+
+  font-size: 23px;
+  font-weight: 800;
 `;
 
 const CreatedDate = styled.span`
-    padding-bottom: 4px;
-    
-    font-size: 11px;
+  padding-bottom: 4px;
 
-    color: #969696;
+  font-size: 11px;
+
+  color: #969696;
 `;
 
-
 const Title = () => {
-    return(
-        <TitleSection>
-            <RoomName>ROOM NAME1</RoomName>
-            <CreatedDate>2021-08-21 개설됨</CreatedDate>
-        </TitleSection>
-    )
-}
+  return (
+    <TitleSection>
+      <RoomName>ROOM NAME1</RoomName>
+      <CreatedDate>2021-08-21 개설됨</CreatedDate>
+    </TitleSection>
+  );
+};
 
 export default Title;
