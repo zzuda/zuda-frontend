03d861c89b2bd21d7561681094eea5e1d6fce7d1
Twince <tjsdn811@gmail.com>
2021-06-27 00:23:46 +0900

    feat: Text Props

--- src/atomic/AdminText/index.jsx
+++ src/atomic/AdminText/index.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const CountText = styled.span`
     color: #717171;
     
-    font-size: 16px;
+    font-size: ${(props) => (props.fSize ? props.fSize : 16)}px;
+    margin-top: ${(props) => (props.marginTop ? props.marginTop : 0)}px;
     font-weight: 600;
 `;
