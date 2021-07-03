7870da3e2a25377010302d256952de3976b19493
ArKc1009 <arkc1009@gmail.com>
2021-07-04 02:46:09 +0900

    feat: file 업로드 기능 구현

--- src/components/Room/FileShare/UploadFile.jsx
+++ src/components/Room/FileShare/UploadFile.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
+import { FileApi } from '../../../Api';
 
 const Container = styled.div`
   width: 600px;
@@ -13,13 +14,34 @@ const Upload = styled.input`
   width: 300px;
   height: 300px;
   background-color: #ddd;
-
 `;
 
 const UploadFile = () => {
+  const fileDom = useRef(null);
+
+  const onClickSubmit = async () => {
+    const file = fileDom.current.files;
+    if (file.length <= 0) {
+      alert('not file!');
+      return;
+    }
+
+    const frm = new FormData();
+    frm.append('roomId', 5);
+    frm.append('files', file);
+
+    try {
+      await FileApi.post('/file/upload', frm);
+    } catch (err) {
+      console.error(err);
+      return;
+    }
+  };
+
   return (
     <Container>
-      <Upload type="file" />
+      <Upload type="file" ref={fileDom} />
+      <button onClick={onClickSubmit}>asd</button>
     </Container>
   );
 };
