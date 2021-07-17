61f6877685c1a417b56e6ff45454d0a02536d3f6
ArKc1009 <arkc1009@gmail.com>
2021-07-17 21:16:07 +0900

    chore: export 추가

--- src/Api/index.jsx
+++ src/Api/index.jsx
@@ -3,7 +3,7 @@ import { getToken } from './tokenManage';
 
 const URL = 'http://localhost:8080';
 
-const Api = axios.create({
+export const Api = axios.create({
   baseURL: URL,
   withCredentials: false,
   headers: {
